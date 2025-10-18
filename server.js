6c70ebd8ac12881e6f31ffc25395c1035a4a72d9
haotqs <hao.tranquangsong@gmail.com>
2025-10-18 21:41:12 +0700

    Test

--- server.js
+++ server.js
@@ -978,6 +978,11 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Force reload endpoint
+app.get('/force-reload', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'force-reload.html'));
+});
+
 // Start server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
